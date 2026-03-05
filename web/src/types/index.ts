8115681e211e6a5db7f6b1085429893801f1b81e
Mel Neves <mel.bichuetti@gmail.com>
2026-03-05 11:59:30 -0300

    Feat: produtos

--- web/src/types/index.ts
+++ web/src/types/index.ts
@@ -6,3 +6,21 @@ export type AllowedRoutes = {
   PATCH?: Role[]
   DELETE?: Role[]
 }
+
+export interface Produto {
+  id: number;
+  nome: string;
+  desc: string;
+  preco: string;
+  imagem: string;
+  categoria: string;
+}
+
+export interface ItemCarrinho extends Produto {
+  quantidade: number;
+}
+
+export interface Usuario {
+  nome: string;
+  email: string;
+}
