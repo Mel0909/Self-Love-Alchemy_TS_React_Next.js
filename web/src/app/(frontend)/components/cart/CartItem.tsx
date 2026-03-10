23e956875311878d51301c261d31918d82df021d
Mel Neves <mel.bichuetti@gmail.com>
2026-03-09 22:05:35 -0300

    fix: Bug do carrinho(+/-)

--- web/src/app/(frontend)/components/cart/CartItem.tsx
+++ web/src/app/(frontend)/components/cart/CartItem.tsx
@@ -8,15 +8,23 @@ export default function CartItem({ item }: any) {
   return (
     <div className={styles.cartItem}>
       <img src={item.imagem} alt={item.nome} />
-      
-      <div className={styles.cartItemInfo}>
-        <h4>{item.nome}</h4>
+
+      <div className={styles.qtSelector}>
+        <button 
+          className={styles.qtBtn} 
+          onClick={() => atualizarQuantidade(item.id, -1)}
+        >
+          -
+        </button>
         
-        <div className={styles.qtSelector}>
-          <button className={styles.qtBtn} onClick={() => atualizarQuantidade(item.id, item.quantidade - 1)}>-</button>
-          <span className={styles.qtNumber}>{item.quantidade}</span>
-          <button className={styles.qtBtn} onClick={() => atualizarQuantidade(item.id, item.quantidade + 1)}>+</button>
-        </div>
+        <span className={styles.qtNumber}>{item.quantidade}</span>
+        
+        <button 
+          className={styles.qtBtn} 
+          onClick={() => atualizarQuantidade(item.id, 1)}
+        >
+          +
+        </button>
       </div>
 
       <button className={styles.removeBtn} onClick={() => removerDoCarrinho(item.id)}>
